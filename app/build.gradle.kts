ebcac33947317acbc50081d237c880512b35c454
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2024-12-27 12:18:53 +0600

    xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
